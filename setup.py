654339d7a625fbeb69090d4fd87d43311f473710
Diogo <diogoferrari@gmail.com>
2020-07-23 10:40:46 -0500

    Added setup.py and README

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='pandasci',
+    url='https://github.com/DiogoFerrari/pandasci',
+    author='Diogo Ferrari',
+    author_email='diogoferrari@gmail.com',
+    # Needed to actually package something
+    packages=['pandasci'],
+    # Needed for dependencies
+    install_requires=['pandas', 'numpy', 'matplotlib'],
+    # *strongly* suggested for sharing
+    version='0.001',
+    # The license can be anything you like
+    license='MIT',
+    description='A set of classes and functions to facilidade EDA in python',
+)
